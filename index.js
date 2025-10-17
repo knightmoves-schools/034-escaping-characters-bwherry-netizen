06c5db9c8338fc9bd3ad49f6403f32ce6dd5d348
Brendon W <bwherry@midland.k12.ia.us>
2025-10-17 17:19:51 +0000

    gg

---
+++ index.js
@@ -0,0 +1,12 @@
+let introduction = 'High how are you,\n' +
+"\t It is such a \"beuatiful day\" outside.\n" +
+'it hasnt been this hot in \'Forever\'.\n\n' +
+'Go outside today\n' +
+'I just might see you :\\'
+
+document.getElementById("result").innerHTML = introduction;
+
+
+
+
+
